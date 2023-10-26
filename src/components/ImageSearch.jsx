1d80cdeb2075c66315fcf34d2cdae676b3a47b14
basadhi <thennakoonbw.20@uom.lk>
2023-10-26 21:51:06 +0530

    cc

--- src/components/ImageSearch.jsx
+++ src/components/ImageSearch.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { BsSearch } from 'react-icons/bs';
+import {BiCamera} from 'react-icons/bi'
 
 function ImageSearch() {
   const [selectedImage, setSelectedImage] = useState(null);
@@ -9,12 +10,29 @@ function ImageSearch() {
     setSelectedImage(file);
   };
 
+  const handleCameraSearch = () => {
+    
+    navigator.mediaDevices
+      .getUserMedia({ video: true })
+      .then((stream) => {
+        // Handle the camera stream,
+      })
+      .catch((error) => {
+        console.error('Camera access error:', error);
+      });
+  };
+
   return (
     <header className='image_header'>
       <div className='image_search'>
         <input type="file" accept="image/*" onChange={handleImageChange} />
         <BsSearch className="search-icon" />
-        <p>Upload an image for search</p>
+        
+
+        <button className="camera-search-button" onClick={handleCameraSearch}>
+          <BiCamera className="camera-icon" /> Camera Search
+        </button>
+        
 
         {selectedImage && (
           <div className='image-preview'>

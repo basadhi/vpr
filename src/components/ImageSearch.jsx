83a9f276220b05bd15f2f20bc89565d03d19473f
basadhi <bashadithennakoon@gmail.com4>
2023-10-31 21:57:55 +0530

    com

--- src/components/ImageSearch.jsx
+++ src/components/ImageSearch.jsx
@@ -1,9 +1,12 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { BsSearch } from 'react-icons/bs';
-import {BiCamera} from 'react-icons/bi'
+import { BiCamera } from 'react-icons/bi';
+import axios from 'axios';
 
 function ImageSearch() {
   const [selectedImage, setSelectedImage] = useState(null);
+  const [similarImageIds, setSimilarImageIds] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   const handleImageChange = (event) => {
     const file = event.target.files[0];
@@ -11,38 +14,91 @@ function ImageSearch() {
   };
 
   const handleCameraSearch = () => {
-    
     navigator.mediaDevices
       .getUserMedia({ video: true })
       .then((stream) => {
-        // Handle the camera stream,
-      })
-      .catch((error) => {
-        console.error('Camera access error:', error);
-      });
-  };
+      //   (async () => {
+      //     const capturedImage = await captureImageFromCamera(stream);
+      //     makeAPICall(capturedImage);
+      //     stream.getTracks().forEach((track) => {
+      //       track.stop();
+      //     });
+      //   })();
+      // })
+      // .catch((error) => {
+      //   console.error('Camera access error:', error);
+      // });
+  
+    })
+    .catch((error) => {
+      console.error('Camera access error:', error);
+    });
+};
+
+const makeAPICall = async (imageData) => {
+  try {
+    const formData = new FormData();
+    formData.append('image', imageData);
+
+    setIsLoading(true);
+
+    const response = await axios.post(
+      'https://huggingface.co/spaces/Thenujan/VPR_deploy',
+      formData
+    );
+
+    if (response.status === 200) {
+      if (response.data && Array.isArray(response.data)) {
+        setSimilarImageIds(response.data);
+      } else {
+        console.error('Invalid response data');
+      }
+    } else {
+      console.error('API request failed with status code', response.status);
+    }
+
+    setIsLoading(false);
+  } catch (error) {
+    console.error('API request failed:', error);
+    setIsLoading(false);
+  }
+};
+
+
+  useEffect(() => {
+    if (selectedImage) {
+      makeAPICall(selectedImage);
+    }
+  }, [selectedImage]);
 
   return (
-    <header className='image_header'>
-      <div className='image_search'>
-        <input type="file" accept="image/*" onChange={handleImageChange} />
-        <BsSearch className="search-icon" />
-        
-
-        <button className="camera-search-button" onClick={handleCameraSearch}>
-          <BiCamera className="camera-icon" /> Camera Search
-        </button>
-        
-
-        {selectedImage && (
-          <div className='image-preview'>
-            <img src={URL.createObjectURL(selectedImage)} alt='Uploaded' />
-          </div>
-        )}
-
-        {/* Additional search bar components can be added here */}
+    <div className="image-search-container">
+      <div className="image_header">
+        <div className="image_search">
+          <input type="file" accept="image/*" onChange={handleImageChange} />
+          <BsSearch className="search-icon" />
+          <button className="camera-search-button" onClick={handleCameraSearch}>
+            <BiCamera className="camera-icon" /> Camera Search
+          </button>
+          {selectedImage && (
+            <div className="image-preview">
+              <img src={URL.createObjectURL(selectedImage)} alt="Uploaded" />
+            </div>
+          )}
+        </div>
       </div>
-    </header>
+      {isLoading && <p>Loading...</p>}
+      {similarImageIds.length > 0 && (
+        <div className="similar-image-ids">
+          <h2>Similar Image IDs:</h2>
+          <ul>
+            {similarImageIds.map((imageId, index) => (
+              <li key={index}>{imageId}</li>
+            ))}
+          </ul>
+        </div>
+      )}
+    </div>
   );
 }
 
